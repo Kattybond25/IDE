88850f7de91f6bf1a729e6b2ebc1b99e3315fbeb
Kattybond25 <kattybond@mail.ru>
2023-09-27 09:42:23 +0300

    game_less20_ update

---
+++ Project_1/game_less 20.py
@@ -0,0 +1,23 @@
+"""Игра угадай число за 20 попыток"""
+import numpy as np
+import random as rd
+rd.seed()
+number = np.random.randint(1, 100) # загадываем число
+# количество попыток
+count=0
+low_limit=1
+high_limit=100
+number = rd.randint(low_limit, high_limit)
+while True and count<20:
+    predict_number = int(input("Угадай число от 1 до 100"))
+    count+=1
+    if predict_number <1 or predict_number>100:
+        print(" Чсило вне диапазона")
+        break
+    if predict_number==number:
+        print(f"угадано число {number} за {count} попыток.")
+        break
+    elif predict_number<number:
+        print ( f'Попытка {count}, ваше число меньше меньше задуманного на {number-predict_number}')
+    else:
+        print (f'Попытка {count}, ваше число больше задуманного на {predict_number-number}')
