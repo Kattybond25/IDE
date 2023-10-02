49046cf2c6eae282d7f322c8d9b016d7cdd4eb03
Kattybond25 <145212169+Kattybond25@users.noreply.github.com>
2023-10-02 23:07:16 +0300

    new_less_20

--- Project_1/game_less 20.py
+++ Project_1/game_less 20.py
@@ -1,23 +1,22 @@
 """Игра угадай число за 20 попыток"""
 import numpy as np
 import random as rd
-rd.seed()
-number = np.random.randint(1, 100) # загадываем число
-# количество попыток
-count=0
-low_limit=1
-high_limit=100
-number = rd.randint(low_limit, high_limit)
-while True and count<20:
-    predict_number = int(input("Угадай число от 1 до 100"))
-    count+=1
-    if predict_number <1 or predict_number>100:
-        print(" Число вне диапазона")
-        break
-    if predict_number==number:
-        print(f"угадано число {number} за {count} попыток.")
-        break
-    elif predict_number<number:
-        print ( f'Попытка {count}, ваше число меньше меньше задуманного на {number-predict_number}')
-    else:
-        print (f'Попытка {count}, ваше число больше задуманного на {predict_number-number}')
+
+def guess_number(target_number):
+    low_limit=1 #нижний предел диапазона чисел
+    up_limit=100 #верхний предел диапазона чисел
+    attempts=0 # счетчик попыток
+    while True:
+        guessed_number=(low_limit+up_limit)//2 # Вычисляем середину диапазона
+        attempts+=1 # увеличиваем счетчик попыток на 1
+        if guessed_number==target_number:
+            return attempts # если предполагаемое число совпадает с целевым, возвращаем количество попыток
+        elif guessed_number<target_number:
+            low_limit=guessed_number+1 # корректируем нижний предел для следующей итерации
+        else:
+            up_limit=guessed_number-1 # корректируем верхний предел для следующей итерации
+target_number=rd.randint(1,100) # генерируем случайное число от 1 до 100
+attempts_needed=guess_number(target_number)
+print(f"{target_number} was found for {attempts_needed} attempts")
+# угадываем число и выводим количество попыток
+    
