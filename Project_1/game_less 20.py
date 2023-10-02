3d2a0a3baf0d02d1a1cc375cd24a0cb9608c4905
Kattybond25 <145212169+Kattybond25@users.noreply.github.com>
2023-10-02 23:21:03 +0300

    add score game

--- Project_1/game_less 20.py
+++ Project_1/game_less 20.py
@@ -18,5 +18,27 @@ def guess_number(target_number):
 target_number=rd.randint(1,100) # генерируем случайное число от 1 до 100
 attempts_needed=guess_number(target_number)
 print(f"{target_number} was found for {attempts_needed} attempts")
+def score_game(guess_function):
+    # устанавливаем количество раундов для проверки
+    number_of_rounds = 1000
+    total_attempts = 0
+    
+    for _ in range(number_of_rounds): # генерируем случайное число для каждого раунда
+        target_number = rd.randint(1, 100)
+        
+        # вызываем функцию угадывания числа и суммируем попытки
+        attempts = guess_function(target_number)
+        total_attempts += attempts
+    
+    average_attempts = total_attempts / number_of_rounds # среднее количество попыток для угадывания
+    print(f"Average number of attempts to guess: {average_attempts}")
+
+# Вызываем функцию для оценки игры
+score_game(guess_number)
+
+
+
+
+
 # угадываем число и выводим количество попыток
     
