ea50dfe8cc9151fbb36ca97b73f1308b1a1c7f60
Kattybond25 <145212169+Kattybond25@users.noreply.github.com>
2023-10-17 10:24:21 +0300

    new

--- Data/Data2/Data_2.py
+++ Data/Data2/Data_2.py
@@ -4,6 +4,6 @@ movies_file = pd.read_csv('Data\Data2\movies.csv', sep=',')
 rating1_file = pd.read_csv('Data\Data2\raitings1.csv', sep=',')
 rating2_file = pd.read_csv('Data\Data2\ratings2.csv', sep=',')
 
-#unique_movies=movies_file['movieID'].nunique()
-#print(unique_movies)
+unique_movies=movies_file['movieID'].nunique()
+print(unique_movies)
 
