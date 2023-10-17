055f272d0b6e4d89baba34fb3ee01367de583161
Kattybond25 <145212169+Kattybond25@users.noreply.github.com>
2023-10-17 10:22:27 +0300

    new_names

---
+++ Data/Data2/Data_2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+dates_file = pd.read_csv('Data\Data2\dates.csv', sep=',')
+movies_file = pd.read_csv('Data\Data2\movies.csv', sep=',')
+rating1_file = pd.read_csv('Data\Data2\raitings1.csv', sep=',')
+rating2_file = pd.read_csv('Data\Data2\ratings2.csv', sep=',')
+
+#unique_movies=movies_file['movieID'].nunique()
+#print(unique_movies)
+
