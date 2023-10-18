c7659018d2ab7c0acfa0896700fd172a821a98e1
Kattybond25 <145212169+Kattybond25@users.noreply.github.com>
2023-10-18 20:24:26 +0300

    some

--- Data/Data2/Data_2.py
+++ Data/Data2/Data_2.py
@@ -1,9 +1,18 @@
 import pandas as pd
-dates_file = pd.read_csv('Data\Data2\dates.csv', sep=',')
-movies_file = pd.read_csv('Data\Data2\movies.csv', sep=',')
-rating1_file = pd.read_csv('Data\Data2\raitings1.csv', sep=',')
-rating2_file = pd.read_csv('Data\Data2\ratings2.csv', sep=',')
+#dates_file = pd.read_csv('Data\Data2\dates.csv', sep=',')
+#print(dates_file.head())
+#movies_file = pd.read_csv('Data\Data2\movies.csv', sep=',')
+#print(movies_file.head())
+rating1_file = pd.read_csv('Data\Data2\ratings1.csv', sep=',')
+print(rating1_file.head())
+#rating2_file = pd.read_csv('Data\Data2\ratings2.csv', sep=',')
+#userId — уникальный идентификатор пользователя, который выставил оценку;
+#movieId — уникальный идентификатор фильма;
+#rating — рейтинг фильма.
+#dates — таблица с датами выставления всех оценок
+#date — дата и время выставления оценки фильму.
+#movies — таблица с информацией о фильмах.
+#unique_movies=movies_file['movieId'].nunique()
+#print(unique_movies)
 
-unique_movies=movies_file['movieID'].nunique()
-print(unique_movies)
 
