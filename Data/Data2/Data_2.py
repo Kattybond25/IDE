59848c13d8971753255195e96726dd0cfd9bd18c
Kattybond25 <145212169+Kattybond25@users.noreply.github.com>
2023-10-18 22:42:49 +0300

    ratings_movie

--- Data/Data2/Data_2.py
+++ Data/Data2/Data_2.py
@@ -1,11 +1,11 @@
 import pandas as pd
-#dates_file = pd.read_csv('Data\Data2\dates.csv', sep=',')
+dates_file = pd.read_csv('Data\Data2\dates.csv', sep=',')
 #print(dates_file.head())
 #movies_file = pd.read_csv('Data\Data2\movies.csv', sep=',')
 #print(movies_file.head())
-rating1_file = pd.read_csv('Data\Data2\ratings1.csv', sep=',')
-print(rating1_file.head())
-#rating2_file = pd.read_csv('Data\Data2\ratings2.csv', sep=',')
+ratings1 = pd.read_csv('Data\Data2\ratings1.csv', sep=',')
+#print(rating1_file.head())
+ratings2 = pd.read_csv('Data\Data2\ratings2.csv', sep=',')
 #userId — уникальный идентификатор пользователя, который выставил оценку;
 #movieId — уникальный идентификатор фильма;
 #rating — рейтинг фильма.
@@ -14,5 +14,9 @@ print(rating1_file.head())
 #movies — таблица с информацией о фильмах.
 #unique_movies=movies_file['movieId'].nunique()
 #print(unique_movies)
-
+#dates_file['date']=pd.to_datetime(dates_file['date'])
+#max_year=dates_file['date'].dt.year.value_counts().idxmax()
+#print(max_year)
+ratings = pd.concat([ratings1, ratings2])
+print(ratings)
 
